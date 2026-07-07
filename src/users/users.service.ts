d2944f8f21ed027e9445037ec1a231de0b553690
Syed Sameer Naqvi <sameernaqvi@yahoo.com>
2026-07-07 12:38:39 +0100

    chore(config): update TypeScript, ESLint, and Prettier configuration

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class UsersService {
@@ -45,52 +45,52 @@ export class UsersService {
       email: 'richard.r@example.com',
       role: 'ADMIN',
     },
-  ];
+  ]
 
   findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     if (role) {
-      return this.users.filter((user) => user.role === role);
+      return this.users.filter((user) => user.role === role)
     }
-    return this.users;
+    return this.users
   }
 
   findOne(id: number) {
-    const user = this.users.find((user) => user.id === id);
-    return user;
+    const user = this.users.find((user) => user.id === id)
+    return user
   }
 
   create(user: {
-    name: string;
-    email: string;
-    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    name: string
+    email: string
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN'
   }) {
-    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
-    const newUser = { id: usersByHighestId[0].id + 1, ...user };
-    this.users.push(newUser);
-    return newUser;
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id)
+    const newUser = { id: usersByHighestId[0].id + 1, ...user }
+    this.users.push(newUser)
+    return newUser
   }
 
   update(
     id: number,
     updatedUser: {
-      name?: string;
-      email?: string;
-      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+      name?: string
+      email?: string
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN'
     },
   ) {
     this.users = this.users.map((user) => {
       if (user.id === id) {
-        return { ...user, ...updatedUser };
+        return { ...user, ...updatedUser }
       }
-      return user;
-    });
-    return this.findOne(id);
+      return user
+    })
+    return this.findOne(id)
   }
 
   delete(id: number) {
-    const removedUser = this.findOne(id);
+    const removedUser = this.findOne(id)
 
-    this.users = this.users.filter((user) => user.id !== id);
-    return removedUser;
+    this.users = this.users.filter((user) => user.id !== id)
+    return removedUser
   }
 }
